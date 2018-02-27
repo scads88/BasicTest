be8ef0a8d7eb8905b874a4651e9cb8c9526bd66a
John McCarthy <john3mccarthy@gmail.com>
2018-02-27 12:40:41 +0000

    changed the debug to false, added scads88.pythonanywhere.com to allowed users, and did some config for static root;; hopefully it'll work

--- test2/test2/settings.py
+++ test2/test2/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vlzk8^x=##t8fg#wuem2tc7q_$^kpug*w#@a8vp02w_rir9*0x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["http://scads88.pythonanywhere.com/"]
 
 
 # Application definition
@@ -119,4 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT=" "
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
